dad3ce1f5e74fd1a8a79456d89fe049e8a8d8a4f
Eliot Reyes <eliotmij@gmail.com>
2020-10-30 10:50:35 -0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+
+	//fetch('https://official-joke-api.appspot.com/random_joke').then(response => response.json()).then(data => console.log(data)).catch(err => console.log(err))
+
+</script>
+
+<main>
+	
+
+
+</main>
+
+<style>
+	
+</style>
