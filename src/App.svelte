c3f231fff2267f0f77866852c4bf989d3bb85442
Eliot Reyes <eliotmij@gmail.com>
2020-10-31 12:10:49 -0700

    progress

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,12 @@
 <script>
+	import Todos from './components/Todos.svelte';
 
-	//fetch('https://official-joke-api.appspot.com/random_joke').then(response => response.json()).then(data => console.log(data)).catch(err => console.log(err))
-
+	let todos = [
+        { id: 1, name: "Create Svelte Starter app", completed: true },
+        { id: 2, name: "Create your first component", completed: true },
+        { id: 3, name: "Complete the rest of the tutorial", completed: false },
+        { id: 4, name: "Check reactiveness", completed: false },
+    ]
 </script>
 
-<main>
-	
-
-
-</main>
-
-<style>
-	
-</style>
+<Todos {todos}/>
